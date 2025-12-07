1d3b6640eb3147309edd6a6d242dcfe6bb121bbf
ShreenidhiHD <shreenidhihaagalagodu@gmail.com>
2025-12-07 14:53:27 +0530

    chore: Add ONNX Runtime Web and associated dependencies. #2

--- examples/test-app/vite.config.ts
+++ examples/test-app/vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -10,7 +11,17 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: '../../node_modules/onnxruntime-web/dist/*.wasm',
+                    dest: '.'
+                }
+            ]
+        })
+    ],
     resolve: {
         alias: {
             'react-live-audio': path.resolve(__dirname, '../../src/index.ts'),
